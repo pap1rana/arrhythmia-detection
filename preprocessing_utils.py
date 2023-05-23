27db482a543f6a9e7d4164fd353213bc561768e4
Min032 <asocial.bastard3@gmail.com>
2023-05-24 00:58:26 +0200

    Added preprocessing step: normalization and centering signal

--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -1,4 +1,5 @@
 import wfdb
+import numpy as np
 from wfdb import processing
 
 
@@ -10,3 +11,11 @@ def resample_record_and_annotations(record, annotations, fs_target):
                                                                                              fs=fs_current,
                                                                                              fs_target=fs_target)
     return multichan_resampled_signal, multichan_resampled_ann
+
+
+def normalize_signal_and_center(signal):
+    normalized_signal = wfdb.processing.normalize_bound(signal, lb=0, ub=1)
+    signal_means = np.mean(normalized_signal, axis=0)
+    centered_signal = normalized_signal - signal_means
+
+    return centered_signal
