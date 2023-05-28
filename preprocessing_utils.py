e22a93da01e29a9704f8ab5e9181743138377c13
Min032 <asocial.bastard3@gmail.com>
2023-05-28 02:32:57 +0200

    Added preprocessing step: baseline wander removal using wavelet transform

--- preprocessing_utils.py
+++ preprocessing_utils.py
@@ -1,5 +1,7 @@
 import wfdb
 import numpy as np
+import bwr
+
 from wfdb import processing
 
 
@@ -19,3 +21,18 @@ def normalize_signal_and_center(signal):
     centered_signal = normalized_signal - signal_means
 
     return centered_signal
+
+
+def remove_baseline_wander_wavelets(signal):
+    signal_ch1, signal_ch2 = signal[:, 0], signal[:, 1]
+
+    baseline_ch1 = bwr.calc_baseline(signal_ch1)
+    baseline_ch2 = bwr.calc_baseline(signal_ch2)
+
+    signal_ch1 = signal_ch1 - baseline_ch1
+    signal_ch2 = signal_ch2 - baseline_ch2
+
+    signal_without_wander = np.column_stack((signal_ch1, signal_ch2))
+
+    return signal_without_wander
+
