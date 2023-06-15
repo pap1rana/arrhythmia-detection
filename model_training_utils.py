63c8de2246fcfc1b78cc77348600eee99071abee
pap1rana <aleksa.papic@gmail.com>
2023-06-15 20:26:51 +0200

    Add input layer

--- model_training_utils.py
+++ model_training_utils.py
@@ -4,12 +4,14 @@ import json
 
 # Function takes betti column (string) from mitDB.csv and returns
 # a list with two lists inside - H0 and H1 betti
+
+
 def fix_betti_string_mitdb(retarded_string):
     string_data = retarded_string[1:-1]
     string_data = (re.sub(r'\s+', ',', string_data.replace("\n", ""))).replace("[,", "[")
 
     # Both of these work:
-    #return json.loads(string_data)
+    # return json.loads(string_data)
     return ast.literal_eval(string_data)
 
 
@@ -19,5 +21,5 @@ def fix_betti_string_svdb(retarded_string):
     string_data = (re.sub(r'\s+', ',', retarded_string.replace("\n", ""))).replace("[,", "[")
 
     # Both of these work:
-    #return json.loads(string_data)
+    # return json.loads(string_data)
     return ast.literal_eval(string_data)
